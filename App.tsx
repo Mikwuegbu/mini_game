f7dab12587612685f944fe2b1b1b615c22dd45fa
Mikwuegbu <mstephen130@yahoo.com>
2025-01-16 18:55:28 +0100

    status bar component

--- App.tsx
+++ App.tsx
@@ -7,6 +7,7 @@ import { Colors } from './screens/utils/colors';
 import GameOverScreen from './screens/GameOverScreen';
 import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
+import { StatusBar } from 'expo-status-bar';
 
 // Keep the splash screen visible while we fetch resources
 SplashScreen.preventAutoHideAsync();
@@ -65,20 +66,23 @@ export default function App() {
 	}
 
 	return (
-		<LinearGradient
-			onLayout={onLayoutRootView}
-			colors={[Colors.primary700, Colors.accent500]}
-			style={styles.rootScreen}
-		>
-			<ImageBackground
-				source={require('./assets/images/background-image.jpg')}
-				resizeMode="cover"
+		<>
+			<StatusBar style="light" />
+			<LinearGradient
+				onLayout={onLayoutRootView}
+				colors={[Colors.primary700, Colors.accent500]}
 				style={styles.rootScreen}
-				imageStyle={styles.backgroundImage}
 			>
-				<SafeAreaView style={styles.rootScreen}>{screen}</SafeAreaView>
-			</ImageBackground>
-		</LinearGradient>
+				<ImageBackground
+					source={require('./assets/images/background-image.jpg')}
+					resizeMode="cover"
+					style={styles.rootScreen}
+					imageStyle={styles.backgroundImage}
+				>
+					<SafeAreaView style={styles.rootScreen}>{screen}</SafeAreaView>
+				</ImageBackground>
+			</LinearGradient>
+		</>
 	);
 }
 

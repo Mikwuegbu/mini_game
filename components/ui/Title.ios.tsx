51c2d3f1772c9520db04bc2ca509228f405fbdd4
Mikwuegbu <mstephen130@yahoo.com>
2025-01-16 18:48:57 +0100

    naming platform specific component

---
+++ components/ui/Title.ios.tsx
@@ -0,0 +1,21 @@
+import { StyleSheet, Text, View } from 'react-native';
+import { Colors } from '../../screens/utils/colors';
+
+const Title = ({ title }: { title: string }) => {
+	return <Text style={styles.title}>{title}</Text>;
+};
+
+export default Title;
+
+const styles = StyleSheet.create({
+	title: {
+		fontFamily: 'open-sans-bold',
+		fontSize: 24,
+		color: 'white',
+		textAlign: 'center',
+		borderWidth: 2,
+		marginHorizontal: 20,
+		borderColor: 'white',
+		padding: 8,
+	},
+});
